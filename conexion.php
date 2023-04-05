4ce2d327f520d6a890edef2fe3dee681d6d39ecd
AndresCotrina <cotrinandres@gmail.com>
2023-04-05 16:13:34 -0500

    cambios connect

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-38.railway.app";
-	$port = "port=8029";
+	$host = "host=containers-us-west-144.railway.app";
+	$port = "port=6706";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=JIcL9swq3Y5O3TS911yU";
+	$password = "password=1p4VbTw2tIiCK09Xt7NA";
 	$db = pg_connect("$host $port $dbname $user $password");
     return $db;
 }
